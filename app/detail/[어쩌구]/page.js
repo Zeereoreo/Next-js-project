615b0759160d18bb3c064a77bdfca0e6f234bc33
Zeereoreo <wltjrcjf12@naver.com>
2023-07-02 15:50:38 +0900

    feat: 게시글 리스트 상세페이지

---
+++ app/detail/[어쩌구]/page.js
@@ -0,0 +1,15 @@
+import { connectDB } from "@/util/database.js"
+import { ObjectId } from "mongodb";
+
+export default async function Detail(props){
+    let db = (await connectDB).db('forum');
+    let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.어쩌구)})
+
+    return(
+    <div>
+        <h4>상세페이지임</h4>
+        <h4>{result.title}</h4>
+        <p>{result.content}</p>
+    </div>
+    )
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,8 +1,10 @@
 import { connectDB } from "@/util/database.js"
+import Link from "next/link";
 
 export default async function List() {
   let db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
+  console.log(result[0]._id)
 
   return (
     <div className="list-bg">
@@ -11,6 +13,7 @@ export default async function List() {
                 <div className="list-item" key={idx}>
                     <h4>{el.title}</h4>
                     <p>{el.content}</p>
+                    <Link href={`/detail/${el._id}`}>상세페이지</Link>
                 </div>
                 )
         })}
