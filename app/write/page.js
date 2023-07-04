4ff2c47e46d5364607728f147be7680db29689c1
Zeereoreo <wltjrcjf12@naver.com>
2023-07-04 18:43:32 +0900

    feat: write page

--- app/write/page.js
+++ app/write/page.js
@@ -3,6 +3,8 @@ export default function Write(){
         <div>
             <h4>글 작성</h4>
             <form action="/api/test" method="POST">
+                <input name="title" placeholder="제목"></input>
+                <input name="title" placeholder="내용"></input>
                 <button type="submit">버튼</button>
             </form>
         </div>
