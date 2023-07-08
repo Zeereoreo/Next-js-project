e06e8a6032926bd89cf58426923b08d40dfea44f
Zeereoreo <wltjrcjf12@naver.com>
2023-07-08 17:42:29 +0900

    feat : 캐싱

--- app/list/page.js
+++ app/list/page.js
@@ -3,7 +3,7 @@ import { connectDB } from "@/util/database.js"
 import Link from "next/link";
 import DetailLink from "./DetailLink";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic' // 강제로 다이나믹렌더링 페이지로 변경
 
 export default async function List() {
   let db = (await connectDB).db('forum');
@@ -16,3 +16,10 @@ export default async function List() {
     </div>
   )
 }
+
+export default async function 페이지(){
+  let result = await fetch('/api/어쩌구', { cache: 'force-cache' })}
+
+  fetch('/URL', { next: { revalidate: 60 } }) 
+
+  fetch('/URL', { cache: 'no-store' }) 
