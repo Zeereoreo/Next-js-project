d6d6e1c54293c2442d290e7cc94992c700eb1c37
Zeereoreo <wltjrcjf12@naver.com>
2023-07-08 17:30:16 +0900

    feat : dynamic rendering

--- app/list/page.js
+++ app/list/page.js
@@ -3,6 +3,8 @@ import { connectDB } from "@/util/database.js"
 import Link from "next/link";
 import DetailLink from "./DetailLink";
 
+export const dynamic = 'force-dynamic'
+
 export default async function List() {
   let db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
