e0b7b4056df1187e777f710b8fa862c6820a44d0
Zeereoreo <wltjrcjf12@naver.com>
2023-07-18 17:21:35 +0900

    예제 완성

--- ex/app/projects/page.js
+++ ex/app/projects/page.js
@@ -8,7 +8,7 @@ export default async function Projects(){
 
     return(
         <Layout>
-            <div className='flex flex-col items-conter justify-center min-h-screen px-5 mb-10 px-6'>
+            <div className='flex flex-col items-conter justify-center min-h-screen px-5 mb-10'>
             <Head>
                 <title>포트폴리오</title>
                 <meta name='description' content='오늘도 화이팅'/>
@@ -18,7 +18,7 @@ export default async function Projects(){
                 총 프로젝트 : 
                 <span className="pl-4 text-blue-500">{projects.results.length}</span>
                 </h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8">
             {projects.results.map((aProject)=>(
                 <ProjectItem data={aProject} key={aProject.id}></ProjectItem>
                 ))}
--- ex/components/projects/project-item.js
+++ ex/components/projects/project-item.js
@@ -7,9 +7,29 @@ export default function ProjectItem({data}){
     const githubLink = data.properties.Github.url
     const Description = data.properties.Description.rich_text[0].plain_text
     const Img = data.cover.external.url
+    const tags = data.properties.Skill.multi_select
+    const start = data.properties.WorkPeriod.date.start
+    const end = data.properties.WorkPeriod.date.end
+
+    const calculatedPeriod = (start, end) => {
+        const startDateStringArray = start.split('-');
+        const endDateStringArray = end.split('-');
+
+        var startDate = new Date(startDateStringArray[0], startDateStringArray[1], startDateStringArray[2]);
+        var endDate = new Date(endDateStringArray[0], endDateStringArray[1], endDateStringArray[2]);
+
+        console.log(`startDate: ${startDate}`)
+        console.log(`endDate: ${endDate}`)
+
+        const diffInMs = Math.abs(endDate - startDate);
+        const result = diffInMs / (1000 * 60 * 60 * 24);
+
+        console.log(`기간 : ${result}`)
+        return result;
+    };
 
     return (
-        <div className="flex flex-col m-3 bg-slate-700 rounded-xl w-full">
+        <div className="project-card">
             <Image 
                 className="rounded-t-xl"
                 src={Img} 
@@ -21,9 +41,17 @@ export default function ProjectItem({data}){
             />
 
             <div className="p-4 flex flex-col">
-            <h1>{title}</h1>
-            <h3>{Description}</h3>
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <h3 className="mt-4 text-xl">{Description}</h3>
             <Link href={githubLink}>Github</Link>
+            <p className="my-1">작업기간 : {start} ~ {end} ({calculatedPeriod(start,end)}일)
+            </p>
+            <div className="flex items-start mt-2">
+                {tags.map((aTag)=>
+                    (<h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={tags.id}>{aTag.name}</h1>)
+                )}
+            </div>
+
             </div>
         </div>
     )
