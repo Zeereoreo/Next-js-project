8a387fd40034000fbb5a2df889b72aceb8bc230e
Zeereoreo <wltjrcjf12@naver.com>
2023-07-18 21:07:41 +0900

    잔디 추가

---
+++ ex/components/Home/gitHub.js
@@ -0,0 +1,45 @@
+'use client'
+
+import GitHubCalendar from 'react-github-calendar';
+
+export default function GitHub() {
+
+    const selectLastHalfYear = (contributions) => {
+
+        // 현재 연/월
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+    
+        // 노출할 기간(달)
+        const shownMonths = 5;
+    
+        return contributions.filter((day) => {
+            const date = new Date(day.date);
+            const monthOfDay = date.getMonth();
+        
+            // 현재 날짜가 6월~12월 사이인 경우
+            if (currentMonth >= 5)
+                return (
+                date.getFullYear() === currentYear &&
+                monthOfDay > currentMonth - shownMonths &&
+                monthOfDay <= currentMonth
+                );
+        
+            // 현재 날짜가 1월~5월인 경우
+            return (
+                // 올해
+                (date.getFullYear() === currentYear && monthOfDay <= currentMonth) ||
+                // 작년
+                (date.getFullYear() === currentYear - 1 &&
+                monthOfDay > currentMonth + 11 - shownMonths)
+            );
+            });
+        };
+        
+        return (
+            <GitHubCalendar username="Zeereoreo"
+            blockSize={12}
+            transformData={selectLastHalfYear}
+            />
+        )
+    }
--- ex/components/Home/hero.js
+++ ex/components/Home/hero.js
@@ -1,11 +1,15 @@
+
 import Animation from '@/components/Home/animation'
 import Link from 'next/link'
+import GitHub from './gitHub'
 
 export default function Hero(){
     return (
         <>
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">안녕하세요.
+            <br className="lg:inline-block"/>
+            &quot;지우석&quot; 입니다.
             <br className="hidden lg:inline-block"/>
                 </h1>
                     <p className="mb-8 leading-relaxed">&quot;조금만 더&quot; 란  생각으로 멈추지 않고
@@ -16,6 +20,7 @@ export default function Hero(){
                 <div className="flex justify-center">
                 <Link href='/projects' className="btn-project">프로젝트 보러가기</Link>
                 </div>
+                <GitHub></GitHub>
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <Animation></Animation>
