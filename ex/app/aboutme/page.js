581d52e3a9425036a9a5b726da589b8925eb866f
Zeereoreo <wltjrcjf12@naver.com>
2023-07-19 20:51:11 +0900

    자기소개 완료

--- ex/app/aboutme/page.js
+++ ex/app/aboutme/page.js
@@ -19,15 +19,30 @@ export default function AboutMe(){
                     <br className="lg:inline-block"/>
                     원하는 것이 무엇인지, 불편한 것이 무엇인지, 결국엔 사용자의 마음을 파악할 줄 알아야 한다고 생각합니다.
                     </p>
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Data를 기반으로 감이 아닌 정보를 바탕으로 행동합니다.
+                <h1 className="title-font sm:text-4xl text-3xl mt-10 mb-4 font-medium text-gray-900">감이 아닌 정보를 바탕으로 행동합니다.
                 <br className="hidden lg:inline-block"/>
                 </h1>
                     <p className="mb-8 leading-relaxed">
                         단순히 감이 아닌 Data로 일을 해왔습니다.
                     <br className="lg:inline-block"/>
-                        
+                        매출을 올리기 위해서 매 시즌 니즈를 파악해서 방법을 바꿔 매출을 올리고,
+                        <br className="lg:inline-block"/>
+                        검색량을 바탕으로 새로운 아이템을 찾고 검증 하였고,                        
                     <br className="lg:inline-block"/>
-                    원하는 것이 무엇인지, 불편한 것이 무엇인지, 결국엔 사용자의 마음을 파악할 줄 알아야 한다고 생각합니다.
+                        직접 커뮤니케이션을 통해 불만들을 접수해 환경을 바꿨습니다.
+                    </p>
+                    <h1 className="title-font sm:text-4xl text-3xl mt-10 mb-4 font-medium text-gray-900">
+                        새로운 것과 배움을 좋아하며, 행동으로 보여줍니다.
+                <br className="hidden lg:inline-block"/>
+                </h1>
+                    <p className="mb-8 leading-relaxed">
+                        보다 좋은 질의 서비스를 찾아 개발자의 길에 들어섰습니다.
+                    <br className="lg:inline-block"/>
+                        더 많은 고객들의 만족을 원하기 때문입니다.
+                        <br className="lg:inline-block"/>
+                        하나씩 Stack을 쌓아 나갈 때마다 살아있음을 느낍니다.                        
+                    <br className="lg:inline-block"/>
+                        이 곳 또한, Next.js를 활용하였고, 현재 진행 중인 것은 typescript로 진행하고 있으며 기대해주세요
                     </p>
         </section>
         </Layout>
