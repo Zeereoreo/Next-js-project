0b93e6d235ff8ece771da5eabb81f7400067a44f
Zeereoreo <wltjrcjf12@naver.com>
2023-08-02 18:49:29 +0900

    project 추가

--- ex/components/footer.js
+++ ex/components/footer.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer(){
     return (
-        <>
+        <> 
         <footer className="bg-gray-100">
         <div className="">
             <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
